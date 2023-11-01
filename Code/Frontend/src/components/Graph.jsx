00da3b1b53917edd87e623d637577ebb76a6a472
c21081270 <LiuF29@cardiff.ac.uk>
2023-11-01 20:57:11 +0000

    dropdown menu added

---
+++ Code/Frontend/src/components/Graph.jsx
@@ -0,0 +1,15 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+export default function Graph() {
+
+    return (
+        <DropdownButton id="dropdown-basic-button" title="Visualisation Graphs">
+            <Dropdown.Item href="#">Heat demand before and after energy efficiency measures</Dropdown.Item>
+            <Dropdown.Item href="#">Breakdown of heat demand before and after energy efficiency measures</Dropdown.Item>
+            <Dropdown.Item href="#">Breakdown of energy efficiency improvement costs</Dropdown.Item>
+            <Dropdown.Item href="#">Half-hourly heat production and gas consumption profiles - Gas boilers</Dropdown.Item>
+            <Dropdown.Item href="#">Half-hourly heat production and gas consumption profiles - Resistance heaters</Dropdown.Item>
+            <Dropdown.Item href="#">Summary of heat demand data across England, Wales and Scotland</Dropdown.Item>
+        </DropdownButton>
+    );
+}
