dc15430ef06fa0ff96652f0a15675a233bfc4b77
Fei Liu <liuf29@cardiff.ac.uk>
2023-11-01 20:45:32 +0000

    Revert "added bootstrap dropdown menu"
    This reverts commit 738c7cc965438b6d10a24ccc74a323a9611e92e5

--- Code/Frontend/src/App.jsx
+++ Code/Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Graph from './components/Graph'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-          </p>
-      <Graph></Graph>
+      </p>
     </>
   )
 }
