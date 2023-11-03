a25b05b5d40162f324fdd5e520843d76447b8877
c21081270 <LiuF29@cardiff.ac.uk>
2023-11-03 10:59:10 +0000

    clean up dropdown menu's code

--- Code/Frontend/src/components/VisualisationsDropdownMenu.jsx
+++ Code/Frontend/src/components/VisualisationsDropdownMenu.jsx
@@ -1,17 +1,20 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { useState } from "react";
 export default function VisualisationsDropdownMenu() {
+    const data = [{ name: 'Heat demand before and after energy efficiency measures'},
+        { name: 'Breakdown of heat demand before and after energy efficiency measures'},
+        { name: 'Breakdown of energy efficiency improvement costs' },
+        { name: 'Half-hourly heat production and gas consumption profiles - Gas boilers' },
+        { name: 'Half-hourly heat production and gas consumption profiles - Resistance heaters'},
+        { name: 'Summary of heat demand data across England, Wales and Scotland' }];
 
+    const dropdownTitle = "Visualisation Graphs"
     return (
         <>
-        <DropdownButton id="dropdown-basic-button" drop="down-centered" title="Visualisation Graphs">
-            <Dropdown.Item href="#">Heat demand before and after energy efficiency measures</Dropdown.Item>
-            <Dropdown.Item href="#">Breakdown of heat demand before and after energy efficiency measures</Dropdown.Item>
-            <Dropdown.Item href="#">Breakdown of energy efficiency improvement costs</Dropdown.Item>
-            <Dropdown.Item href="#">Half-hourly heat production and gas consumption profiles - Gas boilers</Dropdown.Item>
-            <Dropdown.Item href="#">Half-hourly heat production and gas consumption profiles - Resistance heaters</Dropdown.Item>
-            <Dropdown.Item href="#">Summary of heat demand data across England, Wales and Scotland</Dropdown.Item>
-            </DropdownButton>
+            <DropdownButton id="dropdown-basic-button" key="dropdown-button" drop="down-centered" title={dropdownTitle}>
+                {data.map((d) => <Dropdown.Item key={d.name} href="#" title={d.name}>{d.name}</Dropdown.Item>)}
+             </DropdownButton>
 
         </>
     );
