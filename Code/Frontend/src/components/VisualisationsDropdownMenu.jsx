f44f61a9cb01e3f6b58689fde456121a07804511
c21081270 <LiuF29@cardiff.ac.uk>
2023-11-03 11:10:32 +0000

    able to change the button title when an dropdown item is selected

--- Code/Frontend/src/components/VisualisationsDropdownMenu.jsx
+++ Code/Frontend/src/components/VisualisationsDropdownMenu.jsx
@@ -1,6 +1,7 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import { useState } from "react";
+import { useState } from 'react';
+
 export default function VisualisationsDropdownMenu() {
     const data = [{ name: 'Heat demand before and after energy efficiency measures'},
         { name: 'Breakdown of heat demand before and after energy efficiency measures'},
@@ -8,12 +9,17 @@ export default function VisualisationsDropdownMenu() {
         { name: 'Half-hourly heat production and gas consumption profiles - Gas boilers' },
         { name: 'Half-hourly heat production and gas consumption profiles - Resistance heaters'},
         { name: 'Summary of heat demand data across England, Wales and Scotland' }];
-
     const dropdownTitle = "Visualisation Graphs"
+
+    const [dropdown, setDropdown] = useState('Visualisation Graphs');
+
+    const handleDropdownChange = (title) => {
+        setDropdown(title)
+    }
     return (
         <>
-            <DropdownButton id="dropdown-basic-button" key="dropdown-button" drop="down-centered" title={dropdownTitle}>
-                {data.map((d) => <Dropdown.Item key={d.name} href="#" title={d.name}>{d.name}</Dropdown.Item>)}
+            <DropdownButton id="dropdown-basic-button" key="dropdown-button" drop="down-centered" title={dropdown}>
+                {data.map((d) => <Dropdown.Item key={d.name} href="#" onClick={() => handleDropdownChange(d.name)}>{d.name}</Dropdown.Item>)}
              </DropdownButton>
 
         </>
