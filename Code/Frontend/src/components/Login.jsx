b24b622dd2dd548a0b85f7c24720d304fde096eb
Alexander Tarling <tarlinga@cardiff.ac.uk>
2023-11-03 15:18:59 +0000

    username and password can be submitted using a form

--- Code/Frontend/src/components/Login.jsx
+++ Code/Frontend/src/components/Login.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import '../App.css'
 
 function Login() {
+    // https://www.w3schools.com/react/react_forms.asp
+    const [inputs, setInputs,] = useState("");
+
+    const handleChange = (event) => {
+        const username = event.target.username;
+        const password = event.target.password;
+        setInputs(values => ({...values, [username]: password}))
+      }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // console.log(inputs)
+        alert(event.target.elements.username.value + " and " + event.target.elements.password.value);
+        // alert("hello");
+    }
+    // end of code
+
     return (
-        <div className="login">
-            <h1>Login Page</h1>
-        </div>
+        <>
+        <h1>Login</h1>
+
+    <form onSubmit={handleSubmit}>
+      <label>User Name:
+        <input type="text" name="username" value={inputs.username} onChange={handleChange}/>
+      </label>
+      <label>Password:
+        <input type="text" name="password" value={inputs.password} onChange={handleChange}/>
+      </label>
+      <input type="submit" />
+    </form>
+
+        </>
     );
 }
 
