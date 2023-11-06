c32d6edba05a0b383e38970931d91be3e3f5c236
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-11-06 15:33:38 +0000

    fixing header and page content not taking up full page

--- Code/Frontend/src/Header.jsx
+++ Code/Frontend/src/Header.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
+import './css/Header.css';
 
 function HeaderLink({ href, text }) {
     return (
-        <a href={href} target="_blank" rel="noreferrer">
+        <a className="header-link" href={href} target="_blank" rel="noreferrer">
             {text}
         </a>
     );
@@ -10,23 +11,23 @@ function HeaderLink({ href, text }) {
 
 HeaderLink.propTypes = {
     href: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired, // Define PropTypes for the 'text' prop
+    text: PropTypes.string.isRequired,
 };
+
 function Header() {
     return (
-        <header>
+        <header className="main-header">
             <div className="navbar-title">
                 <h1>Modular Heat and Cooling Report</h1>
             </div>
             <nav className="navbar">
                 <ul className="nav-links">
-                    <HeaderLink href="/About" text="About" />
-                    <HeaderLink href="visualisations" to="/visualisations" text="Visualizations" />
-                    <HeaderLink href="/login" text="Login" />
+                    <li><HeaderLink href="/About" text="About" /></li>
+                    <li><HeaderLink href="/visualisations" text="Visualisations" /></li>
+                    <li><HeaderLink href="/login" text="Login" /></li>
                 </ul>
             </nav>
         </header>
-
     );
 }
 
