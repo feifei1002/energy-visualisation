874e0cfc3563770141bab04ca20f9c4b81a399db
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-11-07 00:01:07 +0000

    uploading csv fully functional, last csv gets saved with suffix _old as a backup, only 1 backup

--- Code/Backend/routes/api/Csv.js
+++ Code/Backend/routes/api/Csv.js
@@ -3,28 +3,47 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 
-//use multer to set up a storage engine
+const fileSystem = require('fs');
+const path = require('path');
+
 const csvStorage = multer.diskStorage({
-    destination: function(request, file, cb) {
-        cb(null, 'uploads/') //
+    destination: function(request, file, callback) {
+        callback(null, '../data/') //make sure directory does exist
     },
-    filename: function(request, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + '.csv')
+    filename: function(request, file, callback) {
+        const originalName = file.originalname;
+        const targetPath = path.join('../data/', originalName);
+
+        if (fileSystem.existsSync(targetPath)) {
+            //if csv exists, rename it by appending _OLD so we can rollback to it incase
+            const oldFileName = path.parse(originalName).name + '_OLD' + path.extname(originalName);
+            const oldFilePath = path.join('../data/', oldFileName);
+
+            //rename the existing file
+            fileSystem.renameSync(targetPath, oldFilePath, (err) => {
+                if (err) throw err;
+            });
+        }
+        // TODO: do some auth stuff later on here on user permissions/session
+
+        //save the new file with the original name
+        callback(null, originalName);
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: csvStorage });
 
-//POST request for the csv file
+//POST request for saving the csv file
 router.post('/upload-csv', upload.single('file'), (request, response) => {
     if (!request.file) {
         return response.status(400).send('No file uploaded.');
     }
 
-    //authenticate user details (login session) before saving csv, then check csv has correct columns then save
+    //multer auto saves the file to the directory we specified above
+    //maybe do some additional checks on columns if fails then revert to old file?
 
-    //let user know file successfully uploaded
-
+    //tell the user that the file has been saved successfully
+    response.send('File has been uploaded and saved successfully.');
 });
 
 module.exports = router;
