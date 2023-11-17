ee637365503ec39a48fa63d9c25ec7a10fff68cd
c21081270 <LiuF29@cardiff.ac.uk>
2023-11-17 19:32:56 +0000

    making x-axis showing correct value of date

--- Code/Frontend/src/components/graphs/ResistanceHeatersHeatProduced.jsx
+++ Code/Frontend/src/components/graphs/ResistanceHeatersHeatProduced.jsx
@@ -1,4 +1,6 @@
-import { ResponsiveAreaBump } from '@nivo/bump';
+import { ResponsiveLine } from '@nivo/line';
+import { ResponsiveAreaBump } from '@nivo/bump'
+import { ResponsiveBump } from '@nivo/bump'
 import React, {useEffect, useState} from "react";
 export default function ResistanceHeatersHeatProduced() {
 
@@ -19,6 +21,7 @@ export default function ResistanceHeatersHeatProduced() {
         // console.log(heatData[0][ "UK_daily_average_OAT_[degrees_C]"]);
     }, []);
 
+    //Extract the needed data and put it into a new list
     const formatData = heatData.map(({"Normalised_Resistance_heater_elec": resHeat, "UK_daily_average_OAT_[degrees_C]": temperature, "index": time}, index) => ({
         index,
         time,
@@ -26,13 +29,21 @@ export default function ResistanceHeatersHeatProduced() {
         resHeat,
     }));
 
-    const formattedDataList = formatData.map((item => (
-        <li key={item.index}>
-            Time: {item.time},
-            Temperature: {item.temperature},
-            Resistance heater heat: {item.resHeat},
-        </li>
+    const formattedDataList = formatData.map((item => ({
+            id: item.temperature,
+            data: [
+                {
+                    x: new Date(item.time), y: item.resHeat
+                }
+            ]
+        }
+        // <li key={item.index}>
+        //     Time: {item.time},
+        //     Temperature: {item.temperature},
+        //     Resistance heater heat: {item.resHeat},
+        // </li>
     )))
+
 
 
 
@@ -41,6 +52,33 @@ export default function ResistanceHeatersHeatProduced() {
     return(
         <>
             <h1>Graph here - Heat produced</h1>
+            <div style={{ width: '100vw', height: 400}}>
+                <ResponsiveLine
+                    data={formattedDataList}
+                    margin={{ top: 40, right: 100, bottom: 40, left: 100 }}
+                    xScale={
+                    { type: 'time',
+                        format: '%Y-%m-%dT%H:%M:%S',
+                        precision: "month",
+                        min: ("2013-01-01T00:00:00"),
+                        max: ("2013-12-31T23:30:00"),
+                    }
+                }
+                    yScale={{ type: 'linear', min: 5, max: 100, stacked: true, reverse: false }}
+                    axisBottom={{
+                        format: '%b %Y',
+                        tickValues: 'every 1 month'
+                    }}
+                    axisTop={null}
+                    axisRight={null}
+                    enableGridX={false}
+                    colors={{ scheme: 'set1' }}
+                    pointSize={5}
+                    pointColor={{ theme: 'background' }}
+                    pointBorderWidth={3}
+                    pointBorderColor={{ from: 'serieColor' }}
+                />
+            </div>
 
             <ul>{formattedDataList}</ul>
         </>
