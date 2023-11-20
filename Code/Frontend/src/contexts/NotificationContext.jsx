13f1dfbe8b0ae9520e73bb9ecb974d6d20ab163f
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-11-20 20:06:33 +0000

    fixed notification not reappearing when called twice on same page without refresh

--- Code/Frontend/src/contexts/NotificationContext.jsx
+++ Code/Frontend/src/contexts/NotificationContext.jsx
@@ -4,18 +4,30 @@ import NotificationComponent from "../components/Notification.jsx";
 export const NotificationContext = createContext();
 
 export const useNotification = () => useContext(NotificationContext);
-
 export const NotificationProvider = ({ children }) => {
     const [message, setMessage] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
 
     const showNotification = (msg) => {
-        setMessage(msg);
+        setMessage('');
+        setIsVisible(false);
+
+        //need to ensure the state of it has been reset
+        setTimeout(() => {
+            setMessage(msg);
+            setIsVisible(true);
+
+            //hides notification after 5 seconds
+            setTimeout(() => {
+                setIsVisible(false);
+            }, 5000);
+        }, 100); //tiny delay to let reset be performed
     };
 
     return (
         <NotificationContext.Provider value={{ showNotification }}>
             {children}
-            <NotificationComponent message={message} />
+            {isVisible && <NotificationComponent message={message} />}
         </NotificationContext.Provider>
     );
 };
