04a4fb93c9fe44fe512a80b5eb26c2d814647f91
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-11-20 21:04:21 +0000

    removed conflicting configs

--- Code/Frontend/jest.config.js
+++ Code/Frontend/jest.config.js
@@ -10,12 +10,15 @@ module.exports = {
     moduleFileExtensions: ["js", "jsx", "json", "node"],
 
     // The test environment that will be used for testing
-    testEnvironment: "node",
+    testEnvironment: "jsdom",
+
+    // Increase the default Jest timeout for longer tests
+    testTimeout: 150000,
 
     // A map from regular expressions to module names or to arrays of module names
     // that allow to stub out resources with a single module
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+        '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js'
     },
 
     // The glob patterns Jest uses to detect test files
@@ -24,6 +27,13 @@ module.exports = {
         "**/?(*.)+(spec|test).[tj]s?(x)"
     ],
 
-    // A preset that is used as a base for Jest's configuration
-    preset: 'react-native',
+    // Transform ignore patterns
+    transformIgnorePatterns: [
+        "!node_modules/"
+    ],
+
+    // Setup files after the environment setup
+    setupFilesAfterEnv: [
+        "<rootDir>/jest.setup.js"
+    ],
 };
