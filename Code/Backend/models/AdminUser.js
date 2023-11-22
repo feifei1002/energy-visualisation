4af58f46b29f60a716459e0347d6c1b85311707a
ericcmilligan <59842926+ericcmilligan@users.noreply.github.com>
2023-11-22 20:54:12 +0000

    Added web admin(admin of admins) user to mongodb database in web admin table.

---
+++ Code/Backend/models/AdminUser.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const adminUserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+
+const AdminUser = mongoose.model('adminUser', adminUserSchema, 'WebAdmin');
+
+module.exports = AdminUser;
