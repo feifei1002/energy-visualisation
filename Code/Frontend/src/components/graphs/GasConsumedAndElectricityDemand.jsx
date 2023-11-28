002f7d7f0e3ee22e87715c0234e003309e2bb674
Alexander Tarling <tarlinga@cardiff.ac.uk>
2023-11-28 13:29:37 +0000

    added temperature to graph, and changed scale to 3 months for trying to fix long load times

--- Code/Frontend/src/components/graphs/GasConsumedAndElectricityDemand.jsx
+++ Code/Frontend/src/components/graphs/GasConsumedAndElectricityDemand.jsx
@@ -8,54 +8,55 @@ export default function GasConsumedAndElectricityDemand({data}) {
     const [showASHPElecLine, setShowASHPElecLine] = useState(true);
     const [showGSHPElecLine, setShowGSHPElecLine] = useState(true);
     const [showGasConsLine, setShowGasConsLine] = useState(true);
+    const [showTempLine, setShowTempLine] = useState(true);
 
     // put data into a new list
     const formatData = (data || []).map(({index, "Normalised_ASHP_elec": ASHeatPumpsElec, "Normalised_GSHP_elec": GSHeatPumpsElec, "Normalised_Gas_boiler_gas": boilerGasConsumption, "UK_daily_average_OAT_[degrees_C]": temperature}, dataIndex) => ({
         index: dataIndex,
         time: new Date(index),
         // temperature,
-        ASHeatPumpsElec,
-        GSHeatPumpsElec,
+        // ASHeatPumpsElec,
+        // GSHeatPumpsElec,
         boilerGasConsumption,
-        temperature
+        // temperature
     }));
 
     // format data from list
     const formattedDataList = [
-        {
-            // for air source heat pumps
-            id: 'electricity consumption for air-source heat pumps',
-            color: 'hsl(219,95%,33%)',
-            data: formatData.map((item) => ({
-                x: item.time,
-                y: item.ASHeatPumpsElec
-            })),
-        },
-        {
-            // for grouns source heat pumps
-            id: 'electricity consumption for ground source heat pumps',
-            color: 'hsl(0,90%,34%)',
-            data: formatData.map((item) => ({
-                x: item.time,
-                y: item.GSHeatPumpsElec
-            })),
-        },
+        // {
+        //     // for air source heat pumps
+        //     id: 'electricity consumption for air-source heat pumps',
+        //     color: 'hsl(219,95%,33%)',
+        //     data: formatData.map((item) => ({
+        //         x: item.time,
+        //         y: item.ASHeatPumpsElec * 100000
+        //     })),
+        // },
+        // {
+        //     // for grouns source heat pumps
+        //     id: 'electricity consumption for ground source heat pumps',
+        //     color: 'hsl(0,90%,34%)',
+        //     data: formatData.map((item) => ({
+        //         x: item.time,
+        //         y: item.GSHeatPumpsElec * 100000
+        //     })),
+        // },
         {
             // gas consumption
             id: 'gas consumption of gas boilers',
             color: 'hsl(276,91%,38%)',
             data: formatData.map((item) => ({
                 x: item.time,
-                y: item.boilerGasConsumption
+                y: item.boilerGasConsumption * 100000
             })),
         },
         // {
-            // id: 'outside temperature',
-            // color: 'hsl(500, 70%, 50%)',
-            // data: formatData.map((item) => ({
-            //     x: item.time,
-            //     y: item.temperature,
-            // })),
+        //     id: 'outside temperature',
+        //     color: 'hsl(138,87%,18%)',
+        //     data: formatData.map((item) => ({
+        //         x: item.time,
+        //         y: item.temperature,
+        //     })),
         // }
     ];
 
@@ -72,6 +73,10 @@ export default function GasConsumedAndElectricityDemand({data}) {
         setShowGasConsLine(!showGasConsLine);
     }
 
+    const handleShowTempLine = () => {
+        setShowTempLine(!showTempLine);
+    }
+
     const filteredData = formattedDataList.filter((dataToFilter) => {
         if(dataToFilter.id === "electricity consumption for air-source heat pumps") {
             return showASHPElecLine;
@@ -81,6 +86,9 @@ export default function GasConsumedAndElectricityDemand({data}) {
         }
         if(dataToFilter.id === "gas consumption of gas boilers") {
             return showGasConsLine;
+        }
+        if(dataToFilter.id === "outside temperature") {
+            return showTempLine;
         }
     })
 
@@ -99,7 +107,7 @@ export default function GasConsumedAndElectricityDemand({data}) {
                             precision: "minute",
                             tickValues: "every 30 minutes",
                             min: new Date("2013-01-01T00:00:00"),
-                            max: new Date("2013-12-31T23:30:00"),
+                            max: new Date("2013-03-31T23:30:00"),
                         }
                     }
                     yScale={{
@@ -118,23 +126,63 @@ export default function GasConsumedAndElectricityDemand({data}) {
                         legendOffset: 35,
                     }}
                     axisLeft={{
-                        legend: "GWh",
+                        legend: "Half-hourly Electricity and Gas/Hydrogen Demand for Gas Boilers (GWh)",
                         legendPosition:"middle",
                         legendOffset: -60,
                     }}
                     axisTop={null}
-                    axisRight={
+                    axisRight={{
                         // legend: "electricity consumption for air-source heat pumps",
                         // legendPosition:"middle",
                         // legendOffset: 35,
-                        null
-                    }
+                        legend: "Temperature (°C)",
+                        legendPosition:"middle",
+                        legendOffset: 40,
+                    }}
                     enableGridX={false}
                     colors={(d) => d.color}
                     pointSize={10}
                     pointColor={{ theme: 'background' }}
                     pointBorderWidth={3}
                     pointBorderColor={{ from: 'serieColor' }}
+                    enableSlices={'x'}
+                    useMesh={true}
+                    sliceTooltip={({ slice }) => (
+                        <div style={{background: 'white', padding: '10px', border: '1px solid #ccc'}}>
+                            <strong>Date:</strong> {slice.points[0].data.xFormatted}<br></br>
+                            {slice.points.map(point => (
+                                <div key={point.id}>
+                                    <strong>{point.serieId}:</strong> {point.data.yFormatted}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                    legends={[
+                        {
+                            anchor: 'top-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 20,
+                            translateY: -40,
+                            itemsSpacing: 0,
+                            itemDirection: 'left-to-right',
+                            itemWidth: 140,
+                            itemHeight: 13,
+                            itemOpacity: 0.75,
+                            symbolSize: 8,
+                            symbolShape: 'circle',
+                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemBackground: 'rgba(0, 0, 0, .03)',
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }]}
+
                 />
             </div><br/><br/>
 
