f28067a714e422ef4f770b38c2153225a3010aa4
c21081270 <LiuF29@cardiff.ac.uk>
2023-11-30 19:28:48 +0000

    added notifications and prevent users that are not login to access the dashboard page

--- Code/Frontend/src/components/LogoutButton.jsx
+++ Code/Frontend/src/components/LogoutButton.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {NotificationContext} from "../contexts/NotificationContext.jsx";
 
 export default function LogoutButton() {
+    const { showNotification } = useContext(NotificationContext);
     let navigate = useNavigate();
 
     const handleLogout = async ( ) => {
         try{
             await axios.post('api/logout');
             localStorage.removeItem('accessToken');
+            showNotification('Logout successful');
             navigate('/')
         }catch (error) {
             console.error('error logging out: ', error)
