b4198057b4879270145d15d12038fdda2e00a157
ericcmilligan <59842926+ericcmilligan@users.noreply.github.com>
2023-12-04 20:19:51 +0000

    Added video run-down for my graph page.

--- Code/Frontend/src/pages/Wiki/WikiHeatDemand.jsx
+++ Code/Frontend/src/pages/Wiki/WikiHeatDemand.jsx
@@ -9,7 +9,7 @@ export default function WikiHeatDemand() {
                         <iframe
                         width="853"
                         height="480"
-                        src={`https://www.youtube.com/embed/{videoid}`}
+                        src={`https://www.youtube.com/embed/SdnGXUX3HEw?si=Ighmgh7Q9j_oEWTk`}
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
