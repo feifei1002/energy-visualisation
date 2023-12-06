4a3a1b752da875643a1cf9f23c7a86740fe53820
Alexander Tarling <tarlinga@cardiff.ac.uk>
2023-12-06 11:06:09 +0000

    the database is connected to before each login test, so tests involving the database that didn't work are now fixed

--- Code/Backend/__tests__/loginAuthentication.test.js
+++ Code/Backend/__tests__/loginAuthentication.test.js
@@ -1,63 +1,56 @@
 const express = require("express");
 const request = require("supertest");
-
+const mongoose = require("mongoose");
 const router = require('../routes/api/Login');
 const app = express();
+// Get MongoDB password from environment variables
+const password = process.env.MONGODB_PASSWORD;
+// Construct the MongoDB URI
+const uri = `mongodb+srv://milliganec:${password}@climatedata.fh5ht06.mongodb.net/ClimateData?retryWrites=true&w=majority`;
+
 app.use(express.json());
 app.use('/api', router);
 
-// const {MongoClient} = require('mongodb');
-// //Get MongoDB password from environment variables
-// const password = process.env.MONGODB_PASSWORD;
-// // Construct the MongoDB URI
-// const uri = `mongodb+srv://milliganec:${password}@climatedata.fh5ht06.mongodb.net/ClimateData?retryWrites=true&w=majority`;
+// backend tests for the login authentication
+describe('post and get request tests for the login authentication', () => {
+    // before each test connects to the database
+    beforeEach(async () => {
+        await mongoose.connect(uri);
+    });
 
-
-// beforeEach((): void => {
-//     jest.setTimeout(60000)
-// });
-
-describe('api tests', () => {
-
-    // let connection;
-    // let db;
-    //
-    // beforeAll(async () => {
-    //     connection = await MongoClient.connect(uri, {
-    //         useNewUrlParser: true,
-    //         // useUnifiedTopology: true,
-    //     });
-    //     db = await connection.db(users);
-    // });
-
+    // closes database connection after each test
+    afterEach(async () => {
+        await mongoose.connection.close();
+    });
 
     // test to show the GET login route returns 404 status because we only have a POST on the /login route
     test('/login route should return 404 Status', async () => {
         await request(app)
+            // get request
             .get('/login')
             .expect(404);
     });
 
+    // post request when no data is sent
     it('post login request with no data inputted', async () => {
+        // post request with express
         const response = await request(app).post('/api/login');
-        // Expect to be 500 status code because user should not be able to input no data
-        expect(response.status).toBe(500);
-    }, 30000);
 
+        // status code is expected to be 401 because user isn't authenticated when no input data
+        expect(response.status).toBe(401);
+    });
+
+    // post request with invalid data
     it('post login request with invalid information inputted', async () => {
 
-        // post request and sends username and password
+        // post request
         const response = await request(app).post('/api/login').send({
-            // invalid username and password for testing
+            // invalid username and password sent with request
             username: 'invalidUsername',
             password: 'invalidPassword',
         });
 
         // 401 status expected because user is not authenticated with the username and password used above
-
-        // actually is 500 because times out fetching data currently
-        // use https://jestjs.io/docs/mongodb to fix database not being connected to before test
-        expect(response.status).toBe(500);
-    }, 30000);      // adds longer timeout because await may take a while
-
+        expect(response.status).toBe(401);
+    });
 });
