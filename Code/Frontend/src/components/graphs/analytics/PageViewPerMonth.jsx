39250a4f9272a5ca6fb4a2f08f46031f845bfd66
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-12-12 14:37:23 +0000

    graphs show now

--- Code/Frontend/src/components/graphs/analytics/PageViewPerMonth.jsx
+++ Code/Frontend/src/components/graphs/analytics/PageViewPerMonth.jsx
@@ -2,56 +2,73 @@ import React, { useState, useEffect } from 'react';
 import { ResponsiveLine } from '@nivo/line';
 
 export default function PageViewPerMonth() {
-
+    const [rawData, setRawData] = useState([]);
+    const [selectedLines, setSelectedLines] = useState({});
     const [data, setData] = useState([]);
+
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const transformDataForGraph = (rawData) => {
+        const lineData = {};
+
+        rawData.forEach(item => {
+            const key = item._id.pageUrl + ' - ' + item._id.eventType;
+            const dateLabel = monthNames[item._id.month - 1] + ' ' + item._id.year;
+
+            if (!lineData[key]) {
+                lineData[key] = { id: key, data: [] };
+            }
+            lineData[key].data.push({ x: dateLabel, y: item.count });
+        });
+
+        return Object.values(lineData);
+    };
+
+
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('http://localhost:8082/api/analytics/pageviews-per-month');
             const result = await response.json();
-            const formattedData = result.map(item => ({
-                date: `${item._id.month}-${item._id.year}`,
-                pageUrl: item._id.pageUrl,
-                count: item.count
-            }));
-            setData(formattedData);
+            setRawData(result);
+            setSelectedLines(result.reduce((acc, item) => {
+                const key = item._id.pageUrl + ' - ' + item._id.eventType;
+                acc[key] = true; // Initially, all lines are selected
+                return acc;
+            }, {}));
         };
 
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const transformedData = transformDataForGraph(rawData);
+        const filteredData = transformedData.filter(line => selectedLines[line.id]);
+        setData(filteredData);
+    }, [rawData, selectedLines]);
+
+    const handleToggle = (lineId) => {
+        setSelectedLines(prev => ({ ...prev, [lineId]: !prev[lineId] }));
+    };
+
     return (
-        <div style={{height: "400px"}}>
-            <ResponsiveLine
-                data={[{id: "PageViews", data: data}]}
-                xScale={{type: 'point'}}
-                yScale={{type: 'linear', stacked: true, min: 'auto', max: 'auto'}}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    orient: 'bottom',
-                    format: '%m-%Y',
-                    tickValues: 'every month',
-                    legend: 'Month',
-                    legendOffset: -12
-                }}
-                axisLeft={{
-                    orient: 'left',
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Page Views',
-                    legendOffset: -40,
-                    legendPosition: 'middle'
-                }}
-                colors={{scheme: 'nivo'}}
-                pointSize={10}
-                pointColor={{theme: 'background'}}
-                pointBorderWidth={2}
-                pointBorderColor={{from: 'serieColor'}}
-                useMesh={true}
-                legends={[]}
-            />
+        <div>
+            <div style={{height: "400px"}}>
+                <ResponsiveLine
+                    data={data}
+                />
+            </div>
+            <div>
+                {Object.keys(selectedLines).map(lineId => (
+                    <div key={lineId}>
+                        <input
+                            type="checkbox"
+                            checked={selectedLines[lineId]}
+                            onChange={() => handleToggle(lineId)}
+                        />
+                        {lineId}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
