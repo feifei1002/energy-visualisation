f1467e547a42c2f8a9bf68d97e2f55254db58f4a
Isaac Vieira <vieirai@cardiff.ac.uk>
2023-12-20 04:10:34 +0000

    fixed environment variables not working in jest tests

---
+++ Code/Backend/jest/setEnvVars.js
@@ -0,0 +1,4 @@
+process.env.MONGODB_PASSWORD="7LY0yFC8F3gfEBCo";
+process.env.AUTH0_CLIENT_SECRET="g9xjxlAoc8UEuTB0K25FF3DPomldaW3IscrF_Iiw0wM0EzJR2k0N875ewRP-m1nq";
+process.env.ACCESS_TOKEN="teamaccesstokensecret";
+process.env.OPENCAGE_API_KEY="48ba85d5f1ac407da58d664e954c7d51";
